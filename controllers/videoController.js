aeb236167c1c18de634f248e49244f8ca7218256
AI-Jiny <key140@naver.com>
2020-11-28 20:16:07 +0900

    #2.21 Home Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,7 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });          //render함수의 첫번째 인자는 템플릿이고, 두 번쨰 인자는 템플릿에 추가할 정보가 담긴 객체
-
+import { videos } from "../db";
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
@@ -8,13 +10,14 @@ export const search = (req, res) => {
 };
 
 export const upload = (req, res) =>
-  res.render("upload", { pageTitle: "Upload" });
+res.render("upload", { pageTitle: "Upload" });
 
 export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+res.render("videoDetail", { pageTitle: "Video Detail" });
 
 export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+res.render("deleteVideo", { pageTitle: "Delete Video" });
+//export const home = (req, res) => res.render("home", { pageTitle: "Home" });          //render함수의 첫번째 인자는 템플릿이고, 두 번쨰 인자는 템플릿에 추가할 정보가 담긴 객체
