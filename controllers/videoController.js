290d5b0e40de77865913a8bd9ef52a95598725d4
AI-Jiny <68456981+AI-Jiny@users.noreply.github.com>
2020-12-01 17:35:59 +0900

    #3.5 Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,14 @@
 import routes from "../routes";
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});                                              //await은 async 안에서만 사용가능
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
