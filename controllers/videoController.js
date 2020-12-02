35269fdb135b8ec12beeacb8cb5be72174a29edd
AI-Jiny <68456981+AI-Jiny@users.noreply.github.com>
2020-12-02 17:48:26 +0900

    #3.11 Installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,11 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
+// Home
+
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});                                              //await은 async 안에서만 사용가능
+    const videos = await Video.find({}).sort({ _id: -1 });                                          //await은 async 안에서만 사용가능
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -11,12 +13,16 @@ export const home = async (req, res) => {
   }
 };
 
+// Search
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;                                                                            //이전에는 const serchingBy = req.query.term; 으로 했었음
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
+
+// Upload
 
 export const getUpload = (req, res) =>
 res.render("upload", { pageTitle: "Upload" });
@@ -34,6 +40,8 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
+// Video Detail
+
 export const videoDetail = async (req, res) => {
   const {
     params: { id }
@@ -45,6 +53,8 @@ export const videoDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
+
+// Edit Video
 
 export const getEditVideo = async (req, res) => {
   const {
@@ -71,13 +81,17 @@ export const postEditVideo = async (req, res) => {
   }
 };
 
+// Delete Video
+
 export const deleteVideo = async (req, res) => {
   const {
     params: { id }
   } = req;
   try {
     await Video.findOneAndRemove({ _id: id });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
   res.redirect(routes.home);
 };
 //export const home = (req, res) => res.render("home", { pageTitle: "Home" });          //render함수의 첫번째 인자는 템플릿이고, 두 번쨰 인자는 템플릿에 추가할 정보가 담긴 객체
