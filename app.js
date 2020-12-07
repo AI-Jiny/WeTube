1b7dd21b67cf819f096338f7b06f8d00d5224e84
AI-Jiny <68456981+AI-Jiny@users.noreply.github.com>
2020-12-07 09:33:36 +0900

    #4.3 ES6 with Webpack

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+
 const app = express();
 
 app.use(helmet());                                            //application이 더 안전하도록 만듦
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());                                      //cookie를 전달받아서 사용할 수있도록 만들어주는 미들웨어
 app.use(bodyParser.json());                                   //사용자가 웹사이트로 전달하는 정보들을 검사하는 미들웨어
 app.use(bodyParser.urlencoded({ extended: true }));           
